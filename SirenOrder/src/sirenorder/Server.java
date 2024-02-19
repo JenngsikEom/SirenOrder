14dbe47024ba0c3f87ccbf1d5f14a526bfb538f0
ljw0814 <leejeongwon8145@gmail.com>
2024-02-19 18:02:21 +0900

    깃 테스트

--- SirenOrder/src/sirenorder/Server.java
+++ SirenOrder/src/sirenorder/Server.java
@@ -1,32 +1,44 @@
 package sirenorder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private static ServerSocket serverSocket = null;
+
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(8080);
-            System.out.println("서버가 시작되었습니다. 클라이언트의 연결을 기다립니다...");
+        System.out.println("--------------------------------------");
+        System.out.println("서버 종료 시 q를 입력하고 Enter키를 입력하세요.");
+        System.out.println("--------------------------------------");
 
-            Socket socket = serverSocket.accept();
-            System.out.println("클라이언트가 연결되었습니다.");
+        // TCP 서버 시작
+        startServer();
+    }
 
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader reader = new BufferedReader(inputStreamReader);
+    public static void startServer() {
+        // 작업 스레드 정의
+        Thread thread = new Thread(() -> {
+            try {
+                // ServerSocket 생성 및 port 바인딩
+                serverSocket = new ServerSocket(50001);
+                System.out.println("[서버] 시작됨");
 
-            String orderInfo = reader.readLine();
-            System.out.println("주문이 접수되었습니다: " + orderInfo);
+                // 연결 수락 및 데이터 통신
+                while (true) {
+                    System.out.println("\n[서버] 연결 요청을 기다림\n");
+                    // 연결 수락
+                    Socket socket = serverSocket.accept();
 
-            socket.close();
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                    // 클라이언트 핸들러 생성 및 스레드 시작
+                    Thread clientHandlerThread = new Thread(new ClientHandler(socket));
+                    clientHandlerThread.start();
+                }
+            } catch (IOException e) {
+                System.out.println("[서버] " + e.getMessage());
+            }
+        });
+        // 스레드 시작
+        thread.start();
     }
 }
