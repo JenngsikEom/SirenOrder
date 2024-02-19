5af59130d47b5c6b3f57f0ca47b5642bcddf6335
jidefine <youjio2000@gmail.com>
2024-02-19 16:24:21 +0900

    커피 종류에 따른 가격 설정 및 결제 방식 구현 완료

--- SirenOrder/src/sirenorder/Payment.java
+++ SirenOrder/src/sirenorder/Payment.java
@@ -1,11 +1,11 @@
 package sirenorder;
 
-import java.io.IOException;
-import java.util.Scanner;
+//import java.io.IOException;
+//import java.util.Scanner;
 
 public class Payment {
 	private String userId;
-	private int money = 10000;
+	public static int money = 10000;
 
 	public Payment(String userId, int money) {
 		this.userId = userId;
@@ -20,37 +20,8 @@ public class Payment {
 		return money;
 	}
 	
-	public String toString() {
-        return userId + "님의 남은 금액: " + money;
-    }
-//	public static void main(String[] args) {
-//		Scanner scanner = new Scanner(System.in);
-//	
-//        int payChoice = 0;
-//        do {
-//			String pay;
-//			System.out.println("결제 수단을 선택해주세요:");
-//			System.out.println("1. 스타벅스 카드");
-//			System.out.println("2. 신용카드");
-//			System.out.println("3. 쿠폰");
-//			payChoice = scanner.nextInt();
-//			switch (payChoice) {
-//			case 1:
-//				pay = "스타벅스 카드";
-//				break;
-//			case 2:
-//				pay = "신용카드";
-//				break;
-//			case 3:
-//				pay = "쿠폰";
-//				break;
-//			default:
-//				System.out.println("잘못된 선택입니다.");
-//				scanner.nextLine();
-//				break;
-//			}
-//        }while(payChoice!=1 || payChoice!=2 || payChoice!=3);
-		
-//        Payment payment = new Payment("userId", 10000);
-//	}
+	public static int leftMoney(int price) {
+		money -= price;
+		return money;
+	}
 }
